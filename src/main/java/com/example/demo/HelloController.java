4530db88b2bad939ab3445aadf4d00878fb52017
Balavignesh2503 <balavignesh5678@gmail.com>
2025-12-19 07:34:24 +0530

    Student Save

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +14,7 @@ import com.example.demo.dto.*;
 
 @RestController
 public class HelloController {
+	List<TaskDto> list = new ArrayList<>();
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello";
@@ -33,6 +34,21 @@ public class HelloController {
 		
 		return new StudentInfoDto(name,age,dept);
 	}
+	
+	@GetMapping("/gettask")
+	public List<TaskDto> get(){
+		return list;
+	}
+	private long idcount =0;
+	@PostMapping("/newtask")
+	public List<TaskDto> insert(@RequestParam TaskDto task){
+		task.setId(idcount++);		
+		list.add(task);
+		return list;	
+		
+	}
+	
+	
 	
 
 }
---
+++ src/main/java/com/example/demo/dto/TaskDto.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto;
+
+public class TaskDto {
+	private long id;
+	private String description;
+	private boolean completed;
+	
+	public TaskDto(long id, String description, boolean completed) {
+		this.id = id;
+		this.description = description;
+		this.completed = completed;
+	}
+	public long getId() {
+		return this.id;
+	}
+	public String getDescription() {
+		return this.description;
+	}
+	public boolean getCompleted() {
+		return this.completed;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setId(boolean completed) {
+		this.completed = completed;
+	}
+	
+
+}
